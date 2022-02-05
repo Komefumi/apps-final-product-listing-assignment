4abd27ba9436cade5ebcc40415b0f5f8c085c9fd
Komefumi <komefumi@protonmail.ch>
2022-02-05 15:54:53 +0530

    1. Create and use randomization utility that returns one of two values - true or false 2. Fix bug in rendering of inventory count - ensure that all three cases are rendered viz. inventory not tracked, positive inventory count, negative inventory count

--- utils/rand.ts
+++ utils/rand.ts
@@ -1,6 +1,11 @@
 import { ABS_MAX_PRICE, ABS_MAX_INVENTORY } from "config";
 import { MightOrMightNotExist } from "types/alias";
 
+function getRandomTrueOrFalse(): boolean {
+  const integerToCheckWith = parseInt(`${Math.random() * 10}`);
+  return !!(integerToCheckWith % 2);
+}
+
 export function selectRandomElementFromArray<T>(itemArray: T[]): T {
   const arrayLength = itemArray.length;
   if (arrayLength < 2) {
@@ -14,23 +19,23 @@ export function selectRandomElementFromArray<T>(itemArray: T[]): T {
 export function maybeSelectRandomElementFromArray<T>(
   itemArray: T[]
 ): MightOrMightNotExist<T> {
-  const itShouldBeGenerated = Math.random() > 0.5;
+  const itShouldBeGenerated = getRandomTrueOrFalse();
   if (itShouldBeGenerated) return selectRandomElementFromArray(itemArray);
   else return undefined;
 }
 
 export function generateRandomPrice(): number {
   const priceRawValue = Math.floor(Math.random() * (ABS_MAX_PRICE + 1));
-  const isPositive = Math.random() > 0.5;
+  const isPositive = getRandomTrueOrFalse();
   if (isPositive) return priceRawValue;
   else return -1 * priceRawValue;
 }
 
 export function maybeGenerateInventoryCount(): MightOrMightNotExist<number> {
-  const itShouldBeGenerated = Math.random() > 0.5;
+  const itShouldBeGenerated = getRandomTrueOrFalse();
   if (!itShouldBeGenerated) return undefined;
   const inventoryRawCount = Math.floor(Math.random() * (ABS_MAX_INVENTORY + 1));
-  const isPositive = Math.random() > 0.5;
+  const isPositive = getRandomTrueOrFalse();
   if (isPositive) return inventoryRawCount;
   else return -1 * inventoryRawCount;
 }
